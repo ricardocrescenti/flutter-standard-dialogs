6a9e076e8cc7590e4dffdc70831c47ee9cba1010
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2020-08-19 22:12:16 -0300

    Changed app name and brightness

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,10 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Standard Dialogs',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        brightness: Brightness.dark,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
